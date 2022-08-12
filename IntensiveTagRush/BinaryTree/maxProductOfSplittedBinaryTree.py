8641f1f7d482746acda5cb4c95e15ff46729c22b
KimGJHC <gongjinghao.cheng.github@gmail.com>
2022-08-12 10:37:16 -0400

    Q1339

---
+++ IntensiveTagRush/BinaryTree/maxProductOfSplittedBinaryTree.py
@@ -0,0 +1,55 @@
+"""
+1339. Maximum Product of Splitted Binary Tree
+
+Given the root of a binary tree, split the binary tree into two subtrees by removing one edge such that the product of the sums of the subtrees is maximized.
+
+Return the maximum product of the sums of the two subtrees. Since the answer may be too large, return it modulo 109 + 7.
+
+Note that you need to maximize the answer before taking the mod and not after taking it.
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def maxProduct(self, root):
+        sum_of_tree = 0
+        self.MOD = 10 ** 9 + 7
+
+        stack = [root]
+        while stack:
+            current = stack.pop()
+            sum_of_tree += current.val
+            if current.left:
+                stack.append(current.left)
+            if current.right:
+                stack.append(current.right)
+
+        max_product = 0
+
+        def postorder(node):
+            nonlocal sum_of_tree
+            nonlocal max_product
+            # return the sum of subtree rooted at node
+            if not node:
+                return 0
+
+            left = postorder(node.left)
+            right = postorder(node.right)
+
+            break_left = left * (sum_of_tree - left)
+            break_right = right * (sum_of_tree - right)
+
+            max_product = max(max_product, break_left, break_right)
+
+            return node.val + left + right
+
+        postorder(root)
+        return max_product % self.MOD
+# solution 1: 2 pass the tree, 1 pass for sum, 1 pass for best product
+# time: O(n)
+# space: O(h)
