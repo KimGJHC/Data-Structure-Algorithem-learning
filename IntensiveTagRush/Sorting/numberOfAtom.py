203853fc204b6c47bdf334cc327925b57edf6d6d
KimGJHC <gongjinghao.cheng.github@gmail.com>
2022-08-19 11:34:43 -0400

    Q726

---
+++ IntensiveTagRush/Sorting/numberOfAtom.py
@@ -0,0 +1,58 @@
+"""
+726. Number of Atoms
+Given a string formula representing a chemical formula, return the count of each atom.
+
+The atomic element always starts with an uppercase character, then zero or more lowercase letters, representing the name.
+
+One or more digits representing that element's count may follow if the count is greater than 1. If the count is 1, no digits will follow.
+
+For example, "H2O" and "H2O2" are possible, but "H1O2" is impossible.
+Two formulas are concatenated together to produce another formula.
+
+For example, "H2O2He3Mg4" is also a formula.
+A formula placed in parentheses, and a count (optionally added) is also a formula.
+
+For example, "(H2O2)" and "(H2O2)3" are formulas.
+Return the count of all elements as a string in the following form: the first name (in sorted order), followed by its count (if that count is more than 1), followed by the second name (in sorted order), followed by its count (if that count is more than 1), and so on.
+
+The test cases are generated so that all the values in the output fit in a 32-bit integer.
+"""
+
+from collections import Counter
+class Solution:
+    def countOfAtoms(self, formula):
+        n = len(formula)
+        stack = [Counter()]
+        i = 0
+        while i < n:
+            char = formula[i]
+            if char == '(':
+                stack.append(Counter())
+                i += 1
+            elif char == ')':
+                top = stack.pop()
+                i += 1
+                i_start = i
+                while i < n and formula[i].isdigit():
+                    i += 1
+                multi = int(formula[i_start: i] or 1)
+                for name, val in top.items():
+                    stack[-1][name] += val * multi
+            else:
+                i_start = i
+                i += 1
+                while i < n and formula[i].islower():
+                    i+= 1
+                name = formula[i_start:i]
+                i_start = i
+                while i < n and formula[i].isdigit():
+                    i += 1
+                multi = int(formula[i_start:i] or 1)
+                stack[-1][name] += multi
+        sorted_name = sorted(stack[-1])
+
+        return ''.join(name + (str(stack[-1][name]) if stack[-1][name] > 1 else '') for name in sorted_name)
+
+# solution: using counter to store info in () + sort
+# time: O(n) where n = len(formula)
+# O(n)
