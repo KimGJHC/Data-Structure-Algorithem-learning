259ab0212722aad8de30ea371d95ce777a0be199
KimGJHC <gongjinghao.cheng.github@gmail.com>
2022-09-08 21:17:54 -0400

    Q678

---
+++ IntensiveTagRush/Greedy/validParenthesisString.py
@@ -0,0 +1,23 @@
+"""
+678. Valid Parenthesis String
+Given a string s containing only three types of characters: '(', ')' and '*', return true if s is valid.
+
+The following rules define a valid string:
+
+Any left parenthesis '(' must have a corresponding right parenthesis ')'.
+Any right parenthesis ')' must have a corresponding left parenthesis '('.
+Left parenthesis '(' must go before the corresponding right parenthesis ')'.
+'*' could be treated as a single right parenthesis ')' or a single left parenthesis '(' or an empty string "".
+"""
+
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        # Note that when we traverse the valid string (without *), we will always have left Parenthesis more than right Parenthesis
+        l, h = 0, 0
+        for char in s:
+            l += 1 if char == '(' else -1
+            h += 1 if char != ')' else -1
+            if h < 0:
+                return False
+            l = max(l, 0)
+        return l == 0
