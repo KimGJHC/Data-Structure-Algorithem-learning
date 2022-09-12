a60fd05ac35c8338bedf984246179f41eb48c816
Kim Cheng <gongjinghao.cheng.github@gmail.com>
2022-09-12 00:03:56 -0400

    Q364

---
+++ ArrayHashing/nestedWeightedSumII.py
@@ -0,0 +1,78 @@
+"""
+364. Nested List Weight Sum II
+ou are given a nested list of integers nestedList. Each element is either an integer or a list whose elements may also be integers or other lists.
+
+The depth of an integer is the number of lists that it is inside of. For example, the nested list [1,[2,2],[[3],2],1] has each integer's value set to its depth. Let maxDepth be the maximum depth of any integer.
+
+The weight of an integer is maxDepth - (the depth of the integer) + 1.
+
+Return the sum of each integer in nestedList multiplied by its weight.
+"""
+
+
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+# class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+import collections
+class Solution:
+    def depthSumInverse(self, nestedList: List[NestedInteger]) -> int:
+        depth = collections.defaultdict(list)
+
+        queue = collections.deque(nestedList)
+        cur_depth = 0
+
+        while queue:
+            cur_depth += 1
+            for i in range(len(queue)):
+                current = queue.popleft()
+                if current.isInteger():
+                    value = current.getInteger()
+                    depth[cur_depth].append(value)
+                else:
+                    for items in current.getList():
+                        queue.append(items)
+
+        res = 0
+        for d in depth:
+            for val in depth[d]:
+                res += val * (cur_depth - d + 1)
+        return res
