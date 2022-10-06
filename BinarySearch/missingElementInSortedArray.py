4cbdad925503cc394145e25ce2bf568d48e7f098
KimGJHC <gongjinghao.cheng.github@gmail.com>
2022-10-06 14:19:00 -0400

    Q1060

---
+++ BinarySearch/missingElementInSortedArray.py
@@ -0,0 +1,22 @@
+"""
+1060. Missing Element in Sorted Array
+Given an integer array nums which is sorted in ascending order and all of its elements are unique and given also an integer k, return the kth missing number starting from the leftmost number of the array.
+"""
+
+class Solution:
+    def missingElement(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        l, r = 0, n - 1
+
+        # find the largest num in nums less than k-1 missing elements
+
+        while l <= r:
+            mid = (l + r) // 2
+
+            missing = nums[mid] - nums[0] - mid
+            if missing <= k - 1:
+                l = mid + 1
+            else:
+                r = mid - 1
+        missing_r = nums[r] - nums[0] - r
+        return nums[r] + k - missing_r
