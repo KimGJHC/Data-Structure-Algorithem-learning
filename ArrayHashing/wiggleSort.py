5b5401e8d8c105fb659c19234f12d30a23db523b
KimGJHC <gongjinghao.cheng.github@gmail.com>
2022-10-16 19:17:30 -0400

    Q280

---
+++ ArrayHashing/wiggleSort.py
@@ -0,0 +1,31 @@
+"""
+280. Wiggle Sort
+Given an integer array nums, reorder it such that nums[0] <= nums[1] >= nums[2] <= nums[3]....
+
+You may assume the input array always has a valid answer.
+"""
+
+
+class Solution:
+    def wiggleSort(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        alternative = -1  # -1 for i >= i-1, 1 for i <= i-1
+
+        n = len(nums)
+        if n <= 1:
+            return nums
+
+        i = 1
+
+        while i < n:
+            if alternative == -1:
+                if nums[i] < nums[i - 1]:
+                    nums[i], nums[i - 1] = nums[i - 1], nums[i]
+            else:
+                if nums[i] > nums[i - 1]:
+                    nums[i], nums[i - 1] = nums[i - 1], nums[i]
+            alternative *= -1
+            i += 1
