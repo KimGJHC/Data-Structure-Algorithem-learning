2d27032877f2e3e3962c7dcf1b3d9dd0c36f5f82
Kim Cheng <gongjinghao.cheng.github@gmail.com>
2022-11-17 14:32:02 -0500

    Q1680

---
+++ BitManipulation/1680concantenationOfConsecutiveBinaryNumber.py
@@ -0,0 +1,22 @@
+"""
+1680. Concatenation of Consecutive Binary Numbers
+Given an integer n, return the decimal value of the binary string formed by concatenating the binary representations of 1 to n in order, modulo 109 + 7.
+
+# basic bit manipulation
+"""
+
+
+class Solution:
+    def concatenatedBinary(self, n: int) -> int:
+        if n == 1:
+            return 1
+        MOD = 10 ** 9 + 7
+
+        res = 1
+        for num in range(2, n + 1):
+            bit_size = len(bin(num)) - 2
+            res <<= bit_size
+            res += num
+            res %= MOD
+
+        return res 
