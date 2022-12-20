ef312c6b83f7b2b23e9a4229757b3f3130d44f3a
KimGJHC <gongjinghao.cheng.github@gmail.com>
2022-12-20 17:58:35 -0500

    Q841 optimize with bit masking

--- Graphs/841KeysAndRooms.py
+++ Graphs/841KeysAndRooms.py
@@ -7,6 +7,7 @@ When you visit a room, you may find a set of distinct keys in it. Each key has a
 Given an array rooms where rooms[i] is the set of keys that you can obtain if you visited room i, return true if you can visit all the rooms, or false otherwise.
 
 # think about the question as graph and use bfs
+# whenever we use set to access existence of element, we can probably use bitmasking to reduce runtime and memory consumption
 """
 
 class Solution:
@@ -25,5 +26,25 @@ class Solution:
                     if key not in visited:
                         queue.append(key)
             if len(visited) == n:
+                return True
+        return False
+
+    def canVisitAllRooms_v2(self, rooms: List[List[int]]) -> bool:
+        n = len(rooms)
+        total = 2 ** n - 1
+        queue = collections.deque([0])
+        visited = 0
+        # 0 means room is not being visited
+        visited |= 1 << 0
+
+        while queue:
+            for i in range(len(queue)):
+                current = queue.popleft()
+
+                for key in rooms[current]:
+                    if visited & (1 << key) == 0:
+                        visited |= 1 << key
+                        queue.append(key)
+            if visited == total:
                 return True
         return False
